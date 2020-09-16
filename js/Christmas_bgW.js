1b0edb30d117962be4e2a78fe9efa7a104666b33
0624006 <55527041+0624006@users.noreply.github.com>
2020-09-16 11:34:37 +0800

    Update Christmas_bgW.js

--- js/Christmas_bgW.js
+++ js/Christmas_bgW.js
@@ -1,30 +1,115 @@
-function gift() {
-	$('#open_present_top').attr("class",function(index, attr){return attr == "open_present_top" ? null : "open_present_top";});
-	$('#present_bgLight').attr("class",function(index, attr){return attr == "present_bgLight" ? null : "present_bgLight";});
-	$('#present_bear').attr("class",function(index, attr){return attr == "present_bear" ? null : "present_bear";});
-	$('#present_card').attr("class",function(index, attr){return attr == "present_card" ? null : "present_card";});
-	$('#present_balloon').attr("class",function(index, attr){return attr == "present_balloon" ? null : "present_balloon";});
+/***********/
+/* Loading */
+/***********/
+document.onreadystatechange = function () {
+	if (document.readyState === "complete") {
+		$('#loading').fadeOut();
+		$('.container').fadeIn();
+		console.log(document.readyState);
+	};
 }
-
-function msg() {
-	$("#msg_bg").fadeToggle(1000);
-	setTimeout(function () {
-		$("#msg_bgD").stop(true, true).animate({
-			opacity: "toggle"
-		}, 0);
-		$("#msg_content").attr("class",function(index, attr){return attr == "msg_contentS" ? null : "msg_contentS";});
-		$(".msg").toggleClass("msg_contentS");
-	}, 200);
-}
-
 
 
 $(function () {
-	let list_all=[["MIS", "09136", "林鈺真"],["MIS", "07076", "陳達韋"],["MIS", "02001", "楊宗原"]];
-	var count_str=0;
-	var d = "0";
-	var bgC = 1;
-	$("#iframe_bg" + bgC).toggleClass("bg");
+	var count_all = 0; //獎項總數量，判斷資料庫有無數量; Y: 撈取最小值; N:預設為0
+	if (count_all != '0') {
+		//當資料庫有數值，重整或誤按關閉就不跳出輸入視窗
+		$('#Fkindex').attr('style', 'opacity:0; z-index:-9999;');
+	};
+
+	// list_all:預設陣列 ; count_str:起始陣列數 ; 加入資料庫後可刪
+	let list_all = [["MIS", "09136", "林鈺真"], ["MIS", "07076", "陳達韋"], ["MIS", "02001", "楊宗原"]];
+	var count_str = 0;
+
+	var d = "0";  //作為判斷enter或方向鍵用
+	var bgC = 1; //背景圖數
+	$("#iframe_bg" + bgC).toggleClass("bg"); //初始背景1
+	var msg_wds = ''; //當前獲獎人字串
+
+	/*****************************/
+	/** function gift & msg動畫 **/
+	/****************************/
+	function gift() {
+		$('#open_present_top').attr("class", function (index, attr) { return attr == "open_present_top" ? null : "open_present_top"; });
+		$('#present_bgLight').attr("class", function (index, attr) { return attr == "present_bgLight" ? null : "present_bgLight"; });
+		$('#present_bear').attr("class", function (index, attr) { return attr == "present_bear" ? null : "present_bear"; });
+		$('#present_card').attr("class", function (index, attr) { return attr == "present_card" ? null : "present_card"; });
+		$('#present_balloon').attr("class", function (index, attr) { return attr == "present_balloon" ? null : "present_balloon"; });
+	}
+	function msg() {
+		$("#msg_bg").fadeToggle(1000);
+		setTimeout(function () {
+			$("#msg_bgD").stop(true, true).animate({
+				opacity: "toggle"
+			}, 0);
+			$("#msg_content").attr("class", function (index, attr) { return attr == "msg_contentS" ? null : "msg_contentS"; });
+			$(".msg").toggleClass("msg_contentS");
+		}, 200);
+	}
+
+	/*************************/
+	/* enter / 點選禮物盒觸發 */
+	/************************/
+	function enter_fu() {
+		//暫存當前獲獎人，與下方顯示當前抽取前五項連動
+		msg_wds = `獎序：0 <br>部門：${list_all[count_str][0]}<br>工號：${list_all[count_str][1]}<br>姓名：${list_all[count_str][2]} `;
+		console.log(msg_wds);
+		document.getElementById('msg').innerHTML = msg_wds;
+
+		//判斷陣列數，換成資料庫可刪
+		if (count_str != list_all.length - 1) {
+			count_str += 1;
+		} else {
+			count_str = 0;
+		}
+
+		console.log("enter");
+		d = "1"; //限制左右方向鍵
+		gift();
+		msg();
+	}
+
+	/***********************/
+	/* 重新抽取當前獎項人選 */
+	/**********************/
+	function new_fu() {
+		gift();
+		msg();
+		//延遲1s自動抽取，此時案enter不會重複觸發
+		setTimeout(function () {
+			enter_fu();
+		}, 1000);
+	}
+
+	/******************/
+	/*** 清空獎項欄位 **/
+	/******************/
+	function btnSuc_fun() {
+		alert("clear");
+		count_all = 0;
+		/* 清除資料庫紀錄抽取編號之資料欄位，並重新整理 */
+		setTimeout(function () {
+			location.reload();
+		}, 500);
+	}
+
+	/********************/
+	/*** 下方前五項紀錄 **/
+	/*******************/
+	function list_emAdd() {
+		var list_emN = document.querySelectorAll("#list_em div");
+		if (list_emN.length == '5') {
+			list_emN[0].remove();
+		}
+		var list_em_t = document.createElement("div");
+		list_em_t.innerHTML = msg_wds;
+		document.getElementById('list_em').appendChild(list_em_t);
+	}
+
+
+	/******************/
+	/**   keycode    **/
+	/******************/
 	document.addEventListener("keyup", event => {
 		if (event.isComposing || event.keyCode === 40 || event.keyCode === 83) {  //按方向鍵"下"
 			console.log(bgC);
@@ -35,67 +120,82 @@ $(function () {
 				bgC += 1;
 			};
 			$("#iframe_bg" + bgC).toggleClass("bg");
-		}else{
+		} else {
 			switch (d) {
 				case "0":
 					// Enter
 					if (event.isComposing || event.keyCode === 13) {
-	
-						var msg_wds=`部門：${list_all[count_str][0]}<br>工號：${list_all[count_str][1]}<br>姓名：${list_all[count_str][2]} `;
-						console.log(msg_wds);
-						document.getElementById('msg').innerHTML=msg_wds;
-	
-						if(count_str != list_all.length-1){
-							count_str+=1;
-						}else{
-							count_str=0;
-						}
-	
-						console.log(d);
-						d = "1";
-						gift();
-						msg();
-						// document.getElementById("msg").innerHTML=d;//替換成得獎者
+						enter_fu();
 						return;
 					}
-	
 					break;
 				case "1":
 					// Next
-					if (event.isComposing || event.keyCode === 39) {
+					if (event.isComposing || event.keyCode === 39 || event.keyCode === 68) {
 						console.log(d);
-						// alert(event.code);
+						list_emAdd();
 						d = "0";
-						// $("#msg_bg").fadeToggle(1000);
 						gift();
 						msg();
 						return;
 					}
 					// New
-					if (event.isComposing || event.keyCode === 37) {					
-						console.log(d);
-						alert("確定重新抽取？");
+					if (event.isComposing || event.keyCode === 37 || event.keyCode === 65) {
+						$('#WR_NEW').click();
 						return;
 					}
 					break;
 				default:
 					break;
 			};
-		};	
-		
+		};
+
 	});
+
 
 	/******************/
 	/**    click     **/
 	/******************/
-	WR_NEW.addEventListener("click", function(){
-		alert("確定重新抽取？");
+	$('#inpt_btn').off('click').on('click', function () {
+		count_all = $('#inpt_txt').val(); //存進資料庫
+		if (count_all <= 1) alert("獎項數量不可小於1，請重新輸入！");
+		else $('#Fkindex').fadeOut();
 	});
-	WR_NEXT.addEventListener("click", function(){
+	$('#WR_NEXT').off('click').on('click', function () {
+		list_emAdd();
 		d = "0";//判斷按鍵enter
 		gift();
 		msg();
 	});
+	$('#open_present_frame').off('click').on('click', function () {
+		enter_fu();
+	});
+	/******************/
+	/** modal通知視窗 **/
+	/******************/
+	$('#myModal').on('show.bs.modal', function (e) {
+		var t = d;//暫存當前按鍵
+		d = "-1"; //禁止modal彈出後按任何按鍵
+		//獲取典籍打開的按鈕
+		var button = $(e.relatedTarget);
+		//根據標籤傳入參數
+		var recipient = button.data('whatever');
+		console.log(recipient);
+		if (recipient === 'star') {
+			md_content.innerHTML = "\
+			<h4><font class='bg-danger text-white'>請注意！</font></br></h4>\
+			此動作將清空已抽取獎項之紀錄，</br>\
+			請再次確認是否要清空資料？";
+			$('#bt_suc').off('click').on('click', btnSuc_fun);
+		};
+		if (recipient === 'WR_NEW') {
+			md_content.innerHTML = "<h4><font class='bg-danger text-white'>請確認是否重新抽取人選！</font></h4>";
+			$('#bt_suc').off('click').on('click', new_fu);
+		};
+		$('#btn_clo').off('click').on('click', function () { d = t; }); // close後返還當前按鍵值
+
+	});
+
 
 	/******************/
 	/**svg tree_light**/
